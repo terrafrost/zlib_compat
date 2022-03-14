c5160f673d13aee2e0fb5ee1fee6f3e4079a4e66
terrafrost <terrafrost@gmail.com>
2022-03-14 15:44:31 -0500

    add test for full flush + finish

--- tests/DeflateTest.php
+++ tests/DeflateTest.php
@@ -97,6 +97,17 @@ class DeflateTest extends PHPUnit\Framework\TestCase
         $this->runTests($orig . $orig, $data);
     }
 
+    public function testTwoBlocks2()
+    {
+        $orig = 'ccdcbbccdadcbcdacaadbacccdcbbaba';
+
+        $context = deflate_init(ZLIB_ENCODING_RAW, ['strategy' => ZLIB_FILTERED]);
+        $data = deflate_add($context, $orig, ZLIB_FULL_FLUSH);
+        $data.= deflate_add($context, $orig, ZLIB_FINISH);
+
+        $this->runTests($orig . $orig, $data);
+    }
+
     private function runTests($expected, $compressed)
     {
         // test decompression the entire string
